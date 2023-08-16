bb28e6e806ff18d863707029c7a63005b3242d9b
xiaotian <wanxiaotian888@gmail.com>
2023-08-16 23:23:39 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    swcPlugins: [
+      [
+        "use-client",
+        {
+          include: ["antd/"],
+        },
+      ],
+    ],
+  },
+}
 
 module.exports = nextConfig
